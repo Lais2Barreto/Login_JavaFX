e14b99d2008a2f2c1a8940331f025797d0165020
Lais Barreto <lais2barreto@gmail.com>
2021-04-11 14:59:39 -0300

    Updated JavaFx

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Tela de Login");
-        primaryStage.setScene(new Scene(root, 300, 330));
+        primaryStage.setScene(new Scene(root, 263, 392));
         primaryStage.show();
     }
 
